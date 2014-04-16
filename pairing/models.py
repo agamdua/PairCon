230c0e94d708d2063c57ca22adcc24fc401025c3
Agam Dua <agamdua@gmail.com>
2014-04-15 21:58:02 -0400

    Added: First draft of models

--- pairing/models.py
+++ pairing/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class PairRequest(models.Model):
+    username = models.CharField(max_length=31)
+    event = models.CharField(max_length=31, default="PyCon 2015")
+    languages = models.CharField(max_length=1023, default="Python/Django")
+    description = models.TextField()
+    expires_in = models.DateTimeField()
